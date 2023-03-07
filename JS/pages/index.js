a8e69e7e860f1c7716f7b03e1f9431b9dedac3e1
Cycle9898 <cycle9898@gmail.com>
2023-03-07 19:18:26 +0100

    refactor: added all DOM builder function to index.js and made some changes to prepare the futur search feature

--- JS/pages/index.js
+++ JS/pages/index.js
@@ -1,3 +1,120 @@
+//Fetch data and create an instanced objects array
+
+const recipeObjectsArray = recipes.map(recipe => new RecipeObject(recipe));
+
+//Build DOM cards
+
+function buildDOMCard(recipes) {
+    //DOM element
+    const recipeGrid = document.querySelector(".recipe-grid");
+    recipeGrid.innerHTML = "";
+
+    //Add built cards to the DOM or error message if there is none
+    if (recipes.length == 0) {
+        const p = document.createElement("p");
+        p.classList.add("no-result");
+        p.innerText = "Aucune recette ne correspond à votre recherche";
+        recipeGrid.appendChild(p);
+    } else {
+        recipes.forEach(recipe => {
+            const builtCard = recipe.getDOMCard();
+            recipeGrid.appendChild(builtCard);
+        });
+    }
+}
+
+//Emptying filter lists
+
+function emptyFilterLists() {
+    //DOM elements
+    const ingredientsFilterList = document.querySelector(".ingredients-dropdown-list");
+    const appliancesFilterList = document.querySelector(".appliances-dropdown-list");
+    const utensilsFilterList = document.querySelector(".utensils-dropdown-list");
+
+    //Emptying DOM elements
+    ingredientsFilterList.innerHTML = "";
+    appliancesFilterList.innerHTML = "";
+    utensilsFilterList.innerHTML = "";
+
+}
+
+//Fill a targeted filter list with data
+
+function fillFilterList(filterList, type) {
+    //DOM elements
+    const ingredientsFilterList = document.querySelector(".ingredients-dropdown-list");
+    const appliancesFilterList = document.querySelector(".appliances-dropdown-list");
+    const utensilsFilterList = document.querySelector(".utensils-dropdown-list");
+
+    //Fill targeted list with formatted tags
+    const formatFilterTag = (filterTag) => filterTag[0].toUpperCase() + filterTag.slice(1);
+
+    filterList.forEach(filter => {
+        const li = document.createElement("li");
+        li.classList.add("py-2", "d-flex", "justify-content-center", "align-items-center", "rounded");
+        li.innerText = formatFilterTag(filter);
+        ////Event listener to add an active filter tag
+        li.addEventListener("click", () => createFilterTag(li.innerText, type));
+
+        //AppendChild() on the good filter
+        switch (type) {
+            case "ingredients":
+                ingredientsFilterList.appendChild(li);
+                break;
+            case "appliances":
+                appliancesFilterList.appendChild(li);
+                break;
+            case "utensils":
+                utensilsFilterList.appendChild(li);
+                break;
+            default:
+                throw "Unknown filter type !";
+        }
+    });
+}
+
+//Create an active filter tag element
+
+function createFilterTag(value, type) {
+    const div = document.createElement("div");
+    div.classList.add("py-3", "px-4", "border-0", "rounded", "mb-3", "me-2", "fw-bolder", "d-flex");
+
+    const p = document.createElement("p");
+    p.classList.add("m-0", "fw-bold");
+    p.innerText = value;
+
+    const crossButton = document.createElement("span");
+    crossButton.classList.add("bi", "bi-x-circle", "ms-1");
+    //Event listener to remove an active filter tag
+    crossButton.addEventListener("click", () => crossButton.parentNode.remove());
+
+    //DOM element
+    const ingredientsFilterTags = document.querySelector(".active-ingredients-filters");
+    const appliancesFilterTags = document.querySelector(".active-appliances-filters");
+    const utensilsFilterTags = document.querySelector(".active-utensils-filters");
+
+    //AppendChild() to the good tag list
+    div.appendChild(p);
+    div.appendChild(crossButton);
+
+    switch (type) {
+        case "ingredients":
+            div.classList.add("ingredient-color");
+            ingredientsFilterTags.appendChild(div);
+            break;
+        case "appliances":
+            div.classList.add("appliance-color");
+            appliancesFilterTags.appendChild(div);
+            break;
+        case "utensils":
+            div.classList.add("utensil-color");
+            utensilsFilterTags.appendChild(div);
+            break;
+        default:
+            throw "Unknown filter type !";
+    }
+}
+
 //Event listeners on filter buttons to toggle filter lists visibility
 
 const filterButtons = document.querySelectorAll(".filter-buttons button");
@@ -28,10 +145,18 @@ document.addEventListener("click", (event) => {
 //Main function
 
 function init() {
-    //Build DOM cards with fetched data on main page
+    //Build recipe cards on main page with fetched data
     buildDOMCard(recipeObjectsArray);
 
-    //Fill filter lists with fetched data
+    //Create Sets of ingredients, appliances and utensils from fetched data
+
+    const ingredientsSet = new Set(recipeObjectsArray.map((recipeObject) => recipeObject.ingredientsOnly).flat());
+    const appliancesSet = new Set(recipeObjectsArray.map(recipeObject => recipeObject.applianceOnly));
+    const utensilsSet = new Set(recipeObjectsArray.map(recipeObject => recipeObject.utensilsOnly).flat());
+
+    //Fill empty filter lists with previous Sets
+    emptyFilterLists();
+
     fillFilterList(ingredientsSet, "ingredients");
     fillFilterList(appliancesSet, "appliances");
     fillFilterList(utensilsSet, "utensils");
